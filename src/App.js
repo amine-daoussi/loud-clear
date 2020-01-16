d2d2924a2a6a43ea4ce930463eb1a789ad3e7bb8
Walid <walid.msallem@gomycode.co>
2020-01-16 21:40:19 +0000

    #update layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Switch,Router, Route } from 'react-router-dom'
 // import routes from 'shared/generalRoutes'
+import Layout from './layout'
 import Home from './pages/home/index'
 import Impressium from './pages/impressium/index'
 import './global.css'
@@ -8,8 +9,10 @@ import './global.css'
 function App (){
   return(
     <div>
-      <Route exact path='/' component={Home}/> 
-      <Route exact path='/impressium' component={Impressium}/> 
+     <Layout>
+       <Route exact path='/' component={Home}/> 
+       <Route exact path='/impressium' component={Impressium}/> 
+     </Layout>
     </div>
   )
 }
