ad56ea3c99a76da76ef41ad632dc5ce72ec7bf5b
Walid <walid.msallem@gomycode.co>
2020-01-17 15:09:13 +0000

    #add-tabs-compo

---
+++ src/pages/rental/exemple/exemple.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Exemple = () => {
+    console.log("lkjhbgvn,jklmù")
+    return (
+        <div style= {{height :'100px'}}>
+             this an exemple component
+             qdsfdgfhgjhgdsqdzqdzgdgfjfhgfd
+        </div>
+    );
+}
+
+export default Exemple;
---
+++ src/pages/rental/message.js
@@ -0,0 +1,18 @@
+import { defineMessages } from 'react-intl'
+
+export const scope = 'app.containers.rental'
+
+export default defineMessages({
+  title1: {
+    id: `${scope}.title1`,
+    defaultMessage: 'Title 1',
+  },
+  title2: {
+    id: `${scope}.title2`,
+    defaultMessage: 'Title 2',
+  },
+  title3: {
+    id: `${scope}.title3`,
+    defaultMessage: 'Title 3',
+  },
+})
