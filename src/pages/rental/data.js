5d8bcf0b7ea06f56bc0df5e912b883a1ad8cd691
Walid <walid.msallem@gomycode.co>
2020-01-20 01:40:21 +0000

    add images gal

--- src/pages/rental/data.js
+++ src/pages/rental/data.js
@@ -1,8 +1,25 @@
+import XS1 from  '../../assets/images/bundle/XS1.jpg'
+import XS2 from  '../../assets/images/bundle/XS2.jpg'
+import S1 from  '../../assets/images/bundle/S1.jpg'
+import S2 from  '../../assets/images/bundle/S2.jpg'
+import M1 from  '../../assets/images/bundle/M1.jpg'
+import M2 from  '../../assets/images/bundle/M2.jpg'
+import M3 from  '../../assets/images/bundle/M3.jpg'
+import L1 from  '../../assets/images/bundle/L1.jpg'
+import L2 from  '../../assets/images/bundle/L2.jpg'
+import L3 from  '../../assets/images/bundle/L3.jpg'
+import XL1 from  '../../assets/images/bundle/XL1.jpg'
+import XL2 from  '../../assets/images/bundle/XL2.jpg'
+import XXL1 from  '../../assets/images/bundle/XXL1.JPG'
+import XXL2 from  '../../assets/images/bundle/XXL2.jpg'
+
+
 export const bundle1 = {
   id: 1,
   title: "XS",
   // image1:XS1,
   // image2:XS2,
+  images : [{original :XS1 },{original :XS2 }],
   ProductListTop: "2 x VH Micro FR",
   ProductListSub: "1 x VH SUB 118R",
   Amping: "1 x Powersoft X8",
@@ -15,6 +32,7 @@ export const bundle2 = {
   title: "S",
   // image1:S1,
   // image2:S2,
+  images : [{original :S1 },{original :S2 }],
   ProductListTop: "2 x VH SM12H",
   ProductListSub: "2 x VH SUB 118R",
   Amping: "1 x Powersoft X8",
@@ -28,6 +46,7 @@ export const bundle3 = {
   // image1:M1,
   // image2:M2,
   // image3:M3,
+  images : [{original :M1 },{original :M2} ,{original :M3 }],
   ProductListTop: "2 x VH F15EV",
   ProductListSub: "2 x VH SUB 218R",
   Amping: "1 x Powersoft X8",
@@ -41,6 +60,7 @@ export const bundle4 = {
   // image1:L1,
   // image2:L2,
   // image3:L3,
+  images : [{original :L1 },{original :L2 },{original :L3 }],
   ProductListTop: "4 x VH F15EV",
   ProductListSub: "4 x VH SUB 218R",
   Amping: "2 x Powersoft X8",
@@ -53,6 +73,7 @@ export const bundle5 = {
   title: "XL",
   // image1:XL1,
   // image2:XL2,
+  images : [{original :XL1 },{original :XL2 } ],
   ProductListTop: "6 x VH 112H+EMD",
   ProductListSub: "6 x VH SUB 218R",
   Amping: "3 x Powersoft X8",
@@ -65,6 +86,7 @@ export const bundle6 = {
   title: "XXL",
   // image1:XXL1,
   // image2:XXL2,
+  images : [{original :XXL1 },{original :XXL2 } ],
   ProductListTop: "8 x VH 112H+EMD",
   ProductListSub: "8 x VH SUB 218R",
   Amping: "4 x Powersoft X8",
